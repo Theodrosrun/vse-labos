ab23b78045894d2aadd092a9d645c1a82a18414d
theodrosrun <theodros.mulugeta@runciter.ch>
2024-11-10 10:41:43 +0100

    Adding assert

--- ex06_formel/src_tb/datapath_assertions.sv
+++ ex06_formel/src_tb/datapath_assertions.sv
@@ -13,7 +13,7 @@ module datapath_assertions(
 );
 
    // Lorsqu’une opération modifiant Y est lancée, un résultat doit être présenté sur OutPort après un certain temps
-   property p_result;
+   property p_result_eventually;
         @(posedge clk)
         disable iff (Wen ==1)
         ((Op == 3'b000) || (Op == 3'b001) || (Op == 3'b011) || (Op == 3'b100))
@@ -21,38 +21,32 @@ module datapath_assertions(
         ##[0:100] (OutPort)
    endproperty
 
-   // Vérification de l'addition (ADD)
-   property p_alu_add;
-      @(posedge clk)
-      disable iff (Wen == 1)
-      (Op == 3'b000) |-> (OutPort == (RAA + RAB)); // ADD : Y = A + B
-   endproperty
-
-   // Vérification de l'AND
-   property p_alu_and;
-      @(posedge clk)
-      disable iff (Wen == 1)
-      (Op == 3'b011) |-> (OutPort == (RAA & RAB)); // AND : Y = A and B
-   endproperty
-
-   // Vérification du MOV
-   property p_alu_mov;
-      @(posedge clk)
-      disable iff (Wen == 1)
-      (Op == 3'b100) |-> (OutPort == RAA); // MOV : Y = A
-   endproperty
-
    // Propriété pour la stabilité de OutPort
    property p_stable_when_not_writing;
       @(posedge clk)
       disable iff (Wen == 1)
-      $stable(OutPort); // Vérifie que OutPort reste stable quand Wen est désactivé
+      $stable(OutPort);
    endproperty
 
+// Vérification des opérations de l'ALU
+property p_alu_operations;
+   logic [7:0] expected_result;
+   @(posedge clk)
+   disable iff (Wen == 1)
+   
+   case (Op)
+      3'b000: expected_result = RAA + RAB;
+      3'b000: expected_result = RAA >> 1;
+      3'b011: expected_result = RAA & RAB
+      3'b100: expected_result = RAA;
+      default: expected_result = 0;
+   endcase
+
+   OutPort == expected_result;
+endproperty
+
    // Assertions pour vérifier les propriétés
-   assert property(p_result);
-   assert property(p_alu_add);
-   assert property(p_alu_and);
-   assert property(p_alu_mov);
+   assert property(p_result_eventually);
    assert property(p_stable_when_not_writing);
+   assert property(p_alu_operations);
 endmodule

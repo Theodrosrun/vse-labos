605089606c3a26d0fde5b446319856c8cbfa7067
theodrosrun <theodros.mulugeta@runciter.ch>
2024-11-20 21:44:18 +0100

    Typo

--- labo2_uart/code/src_tb/avalon_agent/avalon_driver.sv
+++ labo2_uart/code/src_tb/avalon_agent/avalon_driver.sv
@@ -47,12 +47,12 @@ class avalon_driver#(int DATASIZE=20, int FIFOSIZE=10);
         automatic avalon_transaction transaction;
         $display("%t [AVL Driver] Start", $time);
 
-        vif.rst_i = 1;
-        vif.address_i = 0;
+        vif.rst_i        = 1;
+        vif.address_i    = 0;
         vif.byteenable_i = 'hf;
-        vif.write_i = 0;
-        vif.writedata_i = 0;
-        vif.read_i = 0;
+        vif.write_i      = 0;
+        vif.writedata_i  = 0;
+        vif.read_i       = 0;
 
         @(posedge vif.clk_i);
         vif.rst_i <= 0;
@@ -72,16 +72,17 @@ class avalon_driver#(int DATASIZE=20, int FIFOSIZE=10);
                     $display("%t [AVL Driver] Handling UART_SEND Transaction:\n%s", $time, transaction.toString());
 
                     // Write transaction on the Avalon bus
-                    vif.address_i = transaction.address;
-                    vif.write_i = 1;
+                    vif.address_i   = transaction.address;
+                    vif.write_i     = transaction.write_i;
                     vif.writedata_i = transaction.writedata_i;
+                    
+                    // Send the transaction to the TX scoreboard
+                    avalon_to_scoreboard_tx_fifo.put(transaction);
+
                     @(posedge vif.clk_i);
-                    vif.write_i = 0;
+                    vif.write_i     = 0;
 
                     $display("%t [AVL Driver] Write Completed: Address=%0d, Data=%0d", $time, transaction.address, transaction.writedata_i);
-
-                    // Optionally send the transaction to the TX scoreboard
-                    avalon_to_scoreboard_tx_fifo.put(transaction);
                 end
 
                 UART_READ: begin
@@ -89,17 +90,17 @@ class avalon_driver#(int DATASIZE=20, int FIFOSIZE=10);
 
                     // Read transaction on the Avalon bus
                     vif.address_i = transaction.address;
-                    vif.read_i = 1;
+                    vif.read_i    = transaction.read_i;
+                    
                     @(posedge vif.clk_i);
-                    vif.read_i = 0;
+                    vif.read_i    = 0;
 
-                    // Capture the read data
                     transaction.readdata_o = vif.readdata_o;
-
-                    $display("%t [AVL Driver] Read Completed: Address=%0d, Data=%0d", $time, transaction.address, transaction.readdata_o);
 
                     // Send the transaction to the RX scoreboard
                     avalon_to_scoreboard_rx_fifo.put(transaction);
+
+                    $display("%t [AVL Driver] Read Completed: Address=%0d, Data=%0d", $time, transaction.address, transaction.readdata_o);
                 end
 
                 // TODO - Add WRITE_REGISTER

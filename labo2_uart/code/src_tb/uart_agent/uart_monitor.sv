4e1ade464b03bc94d9c813fc3cffef9435c12fac
theodrosrun <theodros.mulugeta@runciter.ch>
2024-11-25 17:26:41 +0100

    Adding uart monitor

--- labo2_uart/code/src_tb/uart_agent/uart_monitor.sv
+++ labo2_uart/code/src_tb/uart_agent/uart_monitor.sv
@@ -1,6 +1,6 @@
 /*******************************************************************************
 HEIG-VD
-Haute Ecole d'Ingenerie et de Gestion du Canton de Vaud
+Haute Ecole d'Ingénierie et de Gestion du Canton de Vaud
 School of Business and Engineering in Canton de Vaud
 ********************************************************************************
 REDS
@@ -31,32 +31,65 @@ Ver   Date        Person     Comments
 
 import objections_pkg::*;
 
+// Monitor class for the UART interface
 class uart_monitor#(int DATASIZE=20, int FIFOSIZE=10);
 
+    // Testcase ID for reference
     int testcase;
 
+    // Timing information for bits
     int ns_per_bit;
 
+    // FIFO for transmitting observed transactions to the scoreboard
     uart_fifo_t uart_to_scoreboard_tx_fifo;
 
+    // Virtual interface for observing UART signals
     virtual uart_itf vif;
 
+    // Run task to monitor and decode UART transactions
     task run;
-        $display("%t [UART Monitor] Start", $time);
+        $display("%t [UART Monitor] Start monitoring UART interface", $time);
 
-        #20;
+        #20; // Delay for stabilization if needed
 
         while (1) begin
             uart_transaction#(DATASIZE, FIFOSIZE) transaction = new;
 
+            // Wait for the start bit (falling edge of tx_o)
             @(negedge vif.tx_o);
 
-            // TODO : Something
+            $display("%t [UART Monitor] Start bit detected", $time);
 
+            // Decode the UART frame
+            repeat (ns_per_bit) @(vif.clk); // Wait for the middle of the start bit
+
+            // Capture data bits
+            for (int i = 0; i < DATASIZE; i++) begin
+                @(posedge vif.clk);
+                transaction.data[i] = vif.tx_o;
+                repeat (ns_per_bit - 1) @(vif.clk); // Wait for next bit's timing
+            end
+
+            // Capture the parity bit
+            @(posedge vif.clk);
+            transaction.parity = vif.tx_o;
+            repeat (ns_per_bit - 1) @(vif.clk);
+
+            // Capture the stop bit
+            @(posedge vif.clk);
+            transaction.stop = vif.tx_o;
+
+            // Check stop bit validity
+            if (transaction.stop !== 1'b1) begin
+                $error("%t [UART Monitor] Invalid stop bit detected", $time);
+            end
+
+            // Send transaction to the scoreboard
             uart_to_scoreboard_tx_fifo.put(transaction);
+
+            $display("%t [UART Monitor] Transaction captured and sent to scoreboard: %s", $time, transaction.toString());
         end
     endtask
-
 
 endclass : uart_monitor
 

8c2277fbe23991385fd89eacbe13ba7eaad42d91
theodrosrun <theodros.mulugeta@runciter.ch>
2024-11-27 22:25:36 +0100

    Update uart driver

--- labo2_uart/code/src_tb/uart_agent/uart_driver.sv
+++ labo2_uart/code/src_tb/uart_agent/uart_driver.sv
@@ -48,32 +48,53 @@ class uart_driver#(int DATASIZE=20, int FIFOSIZE=10);
         $display("%t [UART Driver] Start", $time);
 
         vif.rx_i = 1;
+
         // Allow setup of the DUV baudrate
         #1000;
 
+        // Loop to process transactions
         while (1) begin
-            automatic logic [DATASIZE:0] data = 0;
-            data[0] = 0;
-
+            // Get a transaction from the sequencer-to-driver FIFO
             objections_pkg::objection::get_inst().drop();
             sequencer_to_driver_fifo.get(transaction);
             objections_pkg::objection::get_inst().raise();
 
             $display("*****************************************************************");
 
-            for (int i = 1; i < DATASIZE + 1; i++) begin
-                data[i] = transaction.data[i-1];
-                $display("%t [UART Driver] Sended data %h", $time, transaction.data[i-1]);
-            end
+            // Handle transactions based on their type
+            case (transaction.transaction_type)
+                SEND: begin
+                    automatic logic [DATASIZE:0] data = 0;
+                    data[0] = 0;
 
-            for (int i = 0; i < DATASIZE + 1; i++) begin
-                #ns_per_bit;
-                vif.rx_i = data[i];
-            end
+                    $display("%t [UART Driver] Handling SEND Transaction:\n%s", $time, transaction.toString());       
 
-            vif.rx_i = 1;
-            $display("%t [UART Driver] Sended data %h", $time, data);
-            uart_to_scoreboard_rx_fifo.put(transaction);
+                    for (int i = 1; i < DATASIZE + 1; i++) begin
+                        data[i] = transaction.data[i-1];
+                    end
+
+                    for (int i = 0; i < DATASIZE + 1; i++) begin
+                        #ns_per_bit;
+                        vif.rx_i = data[i];
+                    end
+
+                    vif.rx_i = 1;
+                    $display("%t [UART Driver] Sended data %h", $time, data[DATASIZE:1]);
+                    uart_to_scoreboard_rx_fifo.put(transaction);
+
+                    $display("[UART Driver] SEND Completed");
+                end
+
+                RECEIVE: begin
+                    $display("%t [UART Driver] Handling RECEIVE Transaction:\n%s", $time, transaction.toString());
+
+                    $display("[UART Driver] RECEIVE Completed");
+                end
+
+                default: begin
+                    $display("%t [AVL Driver] Unknown Transaction Type:\n%s", $time, transaction.toString());
+                end
+            endcase
         end
 
         $display("%t [UART Driver] End", $time);

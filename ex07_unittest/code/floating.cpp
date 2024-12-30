cad8606889fd3f3b1163ecc1a6785c5a64733fc2
Theodros Mulugeta <theodros.mulugeta@runciter.ch>
2024-12-30 20:26:20 +0100

    Update

--- ex07_unittest/code/floating.cpp
+++ ex07_unittest/code/floating.cpp
@@ -8,6 +8,23 @@
 ///
 TEST(Floating, testRounding) {
     // Add your code here
+    float value1 = 0.1f + 0.2f; // Due to floating-point precision, this may not be exactly 0.3
+    float value2 = 0.3f; // Directly defined as 0.3
+
+    // Check if they are almost equal
+    EXPECT_FLOAT_EQ(value1, value2);
+
+    // Additional comparisons to test floating-point rounding
+    float value3 = 1.0f / 3.0f; // Approximation of 1/3
+    float value4 = 0.33333334f; // Close approximation of 1/3
+
+    EXPECT_FLOAT_EQ(value3, value4);
+
+    // Check an intentional failure case
+    float value5 = 0.1234567f;
+    float value6 = 0.1234568f; // Very close but not exactly equal
+
+    EXPECT_FLOAT_EQ(value5, value6); // This may still pass depending on precision
 }
 
 ///
